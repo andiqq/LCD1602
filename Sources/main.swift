a8c922ddcb6044a55e35a14c9c39a5800e265080
andiqq <andreas@plagens.eu>
2025-08-10 16:29:56 +0200

    Swift 6

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,44 +7,64 @@ let i2cs = SwiftyGPIO.hardwareI2Cs(for: .RaspberryPi4)!
 let i2c = i2cs[1]
 let lcdAddress = 0x27
 
-
-func sendByte(value: UInt8, mode: UInt8) {
+func sendByte(value: UInt8, mode: UInt8) async { 
     let highNibble = mode | (value & 0xF0) | 0x08 // High nibble with backlight on
     let lowNibble = mode | ((value << 4) & 0xF0) | 0x08 // Low nibble with backlight on
 
-    i2c.writeByte(lcdAddress, command: 0x00, value: highNibble)
-    toggleEnable(bits: highNibble)
-
-    i2c.writeByte(lcdAddress, command: 0x00, value: lowNibble)
-    toggleEnable(bits: lowNibble)
+    await sendBits(highNibble)
+    await sendBits(lowNibble)
 }
 
-func toggleEnable(bits: UInt8) {
-    usleep(500)
-    i2c.writeByte(lcdAddress, command: 0x00, value: (bits | 0x04)) // Enable bit high
-    usleep(500)
-    i2c.writeByte(lcdAddress, command: 0x00, value: (bits & ~0x04)) // Enable bit low
-    usleep(500)
+@MainActor
+func sendBits(_ bits: UInt8) {
+    i2c.writeByte(lcdAddress, value: (bits | 0x04)) // Enable bit high
+    usleep(50)
+    i2c.writeByte(lcdAddress, value: (bits & ~0x04)) // Enable bit low
+    usleep(50)
 }
 
-func lcdInit() {
-    sendByte(value: 0x33, mode: 0x00) // Initialize
-    sendByte(value: 0x32, mode: 0x00) // Set 4bit mode
-    sendByte(value: 0x06, mode: 0x00) // Cursor move direction
-    sendByte(value: 0x0C, mode: 0x00) // Turn cursor off
-    sendByte(value: 0x28, mode: 0x00) // 2 line display
-    sendByte(value: 0x01, mode: 0x00) // Clear display
+func lcdInit() async {
+    await sendByte(value: 0x33, mode: 0x00) // Initialize
+    await sendByte(value: 0x32, mode: 0x00) // Set 4bit mode
+    await sendByte(value: 0x06, mode: 0x00) // Cursor move direction
+    await sendByte(value: 0x0C, mode: 0x00) // Turn cursor off
+    await sendByte(value: 0x28, mode: 0x00) // 2 line display
+    await sendByte(value: 0x01, mode: 0x00) // Clear display
 }
 
-func lcdString(message: String, line: Int) {
+func lcdString(message: String, line: Int) async {
     let pos: UInt8 = line == 1 ? 0x80 : 0xC0
-    sendByte(value: pos, mode: 0x00)
+    await sendByte(value: pos, mode: 0x00)
     for char in message.utf8 {
-        sendByte(value: char, mode: 0x01)
+        await sendByte(value: char, mode: 0x01)
     }
 }
 
-lcdInit()
-lcdString(message: "Hello, World!", line: 1)
-lcdString(message: "LCD1602 I2C", line: 2)
+func getTime() -> String {
+    let date = Date.now
+    return date.formatted(.dateTime.hour().minute().second().locale(Locale(identifier: "de-DE")))
+}
+
+func getDate() -> String {
+    let date = Date.now
+    return date.formatted(.dateTime.day().month(.twoDigits).year().locale(Locale(identifier: "de-DE")))
+}
+
+func getDateTime() -> String {
+    let currentDate = Date()
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    return formatter.string(from: currentDate)
+}
+
+
+
+await lcdInit()
+await lcdString(message: getDate(), line: 1)
+while true {
+    await lcdString(message: getTime(), line: 2)
+    usleep(1000000)
+}
+
+
 
